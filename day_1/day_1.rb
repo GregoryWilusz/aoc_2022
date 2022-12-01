bc6fbd6a9defcbb4febcd2ddef140f216d87c5fe
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2022-12-01 18:50:59 +0100

    Day 1, part 2

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-def max_total_calories(elves_items_calories_string)
+def total_calories(elves_items_calories_string)
   elves_items_calories_string.split(/\n/)
                              .slice_before('')
                              .map { |array| array[1..].map(&:to_i).inject(:+) }
-                             .max
+end
+
+def max_total_calories(elves_items_calories_string)
+  total_calories(elves_items_calories_string).max
+end
+
+def total_calories_of_top_three_elves(elves_items_calories_string)
+  total_calories(elves_items_calories_string).sort.reverse.take(3).sum
 end
 
 elves_items_calories_string = File.read('./day_1/elves_calories_items_list.txt')
-max_total_calories(elves_items_calories_string)
+p max_total_calories(elves_items_calories_string)
+p total_calories_of_top_three_elves(elves_items_calories_string)
--- spec/day_1/day_1_spec.rb
+++ spec/day_1/day_1_spec.rb
@@ -5,7 +5,15 @@ require_relative '../../day_1/day_1'
 RSpec.describe 'Max Total Calories' do
   let(:example_elves_items_calories) { "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000" }
 
-  it 'calculates max total calories carried by some Elf' do
-    expect(max_total_calories(example_elves_items_calories)).to eq 24_000
+  describe 'Max Total Calories Of The Single Elf' do
+    it 'calculates max total calories carried by a single Elf' do
+      expect(max_total_calories(example_elves_items_calories)).to eq 24_000
+    end
+  end
+
+  describe 'Max Total Calories Of The Top Three Elves' do
+    it 'calculates max total calories carried by three Elves' do
+      expect(total_calories_of_top_three_elves(example_elves_items_calories)).to eq 45_000
+    end
   end
 end
