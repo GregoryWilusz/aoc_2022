8867bb7c3dd0c0c630c102223c88d5e69103ba58
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2022-12-02 20:20:07 +0100

    Day 2, part 1, refactoring #2

--- day_2/day_2.rb
+++ day_2/day_2.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-# Rock (A) defeats Scissors (C-Z),
-# Scissors (C) defeats Paper (B-Y),
-# and Paper (B) defeats Rock (A-X).
-# X for Rock (A), Y for Paper (B), and Z for Scissors (C)
-ROUND_RESULT = {
+ROUND_OUTCOME = {
   'A X' => 3,
   'B X' => 0,
   'C X' => 6,
@@ -16,7 +12,7 @@ ROUND_RESULT = {
   'C Z' => 3
 }.freeze
 
-SHAPE_RESULT = {
+SHAPE_SCORE = {
   'X' => 1,
   'Y' => 2,
   'Z' => 3
@@ -25,7 +21,8 @@ SHAPE_RESULT = {
 def total_score(encrypted_rounds_with_opponents_choices)
   encrypted_rounds_with_opponents_choices
     .split(/\n/)
-    .map { |pair| [ROUND_RESULT[pair], SHAPE_RESULT[pair[-1]]].sum }
+    .tally
+    .map { |sequence, occurrences| (ROUND_OUTCOME[sequence] + SHAPE_SCORE[sequence[-1]]) * occurrences }
     .sum
 end
 
