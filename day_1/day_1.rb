a4ea2d08d4a1fc4e2a82a2d8f04b1574392702f0
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2022-12-02 23:10:39 +0100

    Day 1, refactoring

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -2,8 +2,9 @@
 
 def total_calories(elves_items_calories_string)
   elves_items_calories_string.split(/\n/)
-                             .slice_before('')
-                             .map { |array| array[1..].map(&:to_i).inject(:+) }
+                             .map(&:to_i)
+                             .slice_before(&:zero?)
+                             .map { |array| array.sum }
 end
 
 def max_total_calories(elves_items_calories_string)
@@ -11,7 +12,7 @@ def max_total_calories(elves_items_calories_string)
 end
 
 def total_calories_of_top_three_elves(elves_items_calories_string)
-  total_calories(elves_items_calories_string).sort.reverse.take(3).sum
+  total_calories(elves_items_calories_string).sort.last(3).sum
 end
 
 elves_items_calories_string = File.read('./day_1/elves_calories_items_list.txt')
