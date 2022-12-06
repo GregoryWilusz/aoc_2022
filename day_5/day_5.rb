88c36d96f35239783ff77cc013f7277d67682dfc
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2022-12-06 23:20:02 +0100

    Day 5, part 2

--- day_5/day_5.rb
+++ day_5/day_5.rb
@@ -27,5 +27,33 @@ def rearrange_crates(drawing)
   stacks.values.map(&:last).join
 end
 
+def rearrange_crates_moving_multiple_at_once(drawing)
+  highest_stack_level = drawing.partition("\n\n").first.split(/\n/).size
+
+  stacks = drawing
+           .lines
+           .take(highest_stack_level)
+           .map { |level| level.chars.each_slice(4).to_a.map { |string_with_stack| string_with_stack[1] } }
+           .transpose
+           .map(&:reverse)
+           .to_h { |stack| [stack[0].to_i, stack[1..].join.strip.chars] }
+
+  procedures = drawing
+               .lines
+               .drop(highest_stack_level + 1)
+               .map { |line| line.scan(/\d+/).map(&:to_i) }
+
+  procedures.each do |procedure|
+    from = stacks[procedure[1]]
+    destination = stacks[procedure[2]]
+
+    cargo = from.pop(procedure[0])
+    destination.push(cargo).flatten!
+  end
+
+  stacks.values.map(&:last).join
+end
+
 drawing = File.read('./day_5/drawing_of_the_starting_stacks_and_rearrangement_procedure.txt')
 p "[Day 5][Part 1]: #{rearrange_crates(drawing)}"
+p "[Day 5][Part 2]: #{rearrange_crates_moving_multiple_at_once(drawing)}"
--- spec/day_5/day_5_spec.rb
+++ spec/day_5/day_5_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe 'Supply Stacks' do
       expect(rearrange_crates(example_drawing)).to eq 'CMZ'
     end
   end
+
+  describe 'Returns The Sequence Of The Top Crates Of Each Stack Moving Multiple Crates At Once' do
+    it 'returns the sequence of the top crates of each stack moving multiple crates at once' do
+      expect(rearrange_crates_moving_multiple_at_once(example_drawing)).to eq 'MCD'
+    end
+  end
 end
