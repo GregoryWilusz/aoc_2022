61ddd5d569f592bcc35971c606392a23b27e2c0a
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2022-12-07 01:10:45 +0100

    Day 6, part 2

--- day_6/day_6.rb
+++ day_6/day_6.rb
@@ -4,5 +4,10 @@ def calculate_marker_position(datastream)
   datastream.chars.each_cons(4).with_index { |sequence, index| return index + 4 if sequence.uniq.size == 4 }
 end
 
+def calculate_message_marker_position(datastream)
+  datastream.chars.each_cons(14).with_index { |sequence, index| return index + 14 if sequence.uniq.size == 14 }
+end
+
 datastream_buffer = File.read('./day_6/datastream.txt')
 p "[Day 6][Part 1]: #{calculate_marker_position(datastream_buffer)}"
+p "[Day 6][Part 2]: #{calculate_message_marker_position(datastream_buffer)}"
--- spec/day_6/day_6_spec.rb
+++ spec/day_6/day_6_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe 'Tuning Trouble' do
       expect(calculate_marker_position(datastream5)).to eq 11
     end
   end
+
+  describe 'Returns The Number Of Characters The First Start-of-message Marker Is Detected' do
+    it 'returns the number of characters the first start-of-message marker is detected', :aggregate_failures do
+      expect(calculate_message_marker_position(datastream1)).to eq 19
+      expect(calculate_message_marker_position(datastream2)).to eq 23
+      expect(calculate_message_marker_position(datastream3)).to eq 23
+      expect(calculate_message_marker_position(datastream4)).to eq 29
+      expect(calculate_message_marker_position(datastream5)).to eq 26
+    end
+  end
 end
